1cf3fead6aa9fadab9e43325afbe0085f3b1ac5d
Pink Shark <esipovich2003@gmail.com>
2022-08-08 22:35:48 +0300

    Puk

---
+++ script.js
@@ -0,0 +1,11 @@
+// const Ulula = confirm('Ты любишь Зелень?')
+// console.log(Ulula)
+// if (Ulula==true) {
+//     document.getElementById('1500a').innerHTML = 'A'
+// } else {
+//     document.getElementsByClassName('g')[0].style.backgroundColor = 'green'
+// }
+const f = (text) => {
+    alert(text)
+}
+f('aboba')
